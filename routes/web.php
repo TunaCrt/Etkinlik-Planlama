a8884bfef739926ceb7a1461fadfc6a91d188380
ibrahim Tuna <tuna.cort52@gmail.com>
2024-05-16 14:42:48 +0300

    Template Parçalandı

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/index', function () {
+    return view('panel.layout.app');
+});
+
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
