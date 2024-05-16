2d76244db2cf6dc565a0be5c37cecb47c9e3308c
ibrahim Tuna <tuna.cort52@gmail.com>
2024-05-16 18:05:40 +0300

    modeller ve migrateler eklendi

---
+++ database/migrations/2024_05_16_122149_create_events_table.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('star_id');
+            $table->unsignedBigInteger('university_id');
+
+            $table->string('name');
+            $table->text('description');
+            $table->dateTime('event_date');
+            $table->timestamps();
+
+
+            $table->foreign('user_id')
+                ->on('users')
+                ->references('id')
+                ->onDelete('cascade');
+
+            $table->foreign('star_id')
+                ->on('stars')
+                ->references('id')
+                ->onDelete('cascade');
+
+            $table->foreign('university_id')
+                ->on('universities')
+                ->references('id')
+                ->onDelete('cascade');
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('events');
+    }
+};
